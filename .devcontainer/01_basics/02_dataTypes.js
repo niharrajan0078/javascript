fca190f17cba1f4f3d292a57e33363372e90ab68
niharrajan0078 <155184148+niharrajan0078@users.noreply.github.com>
2023-12-29 20:09:09 +0000

    data Types

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treate all newer code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log ("Nihar")
+
+let name = "Nihar"
+let age = 22
+let isLoggedIn =false
+let state;
+
+// primitive Data Types :-
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => false/true
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+ 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
